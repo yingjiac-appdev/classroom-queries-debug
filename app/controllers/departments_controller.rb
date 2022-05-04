8801bdde319b18ecb72555eae30c046cf335b82c
yingjiac <yingjiac@uchicago.edu>
2022-05-04 18:17:33 +0000

    Finished all debugging

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -12,7 +12,7 @@ class DepartmentsController < ApplicationController
 
     matching_departments = Department.where({ :id => the_id })
 
-    @the_department = matching_departments
+    @the_department = matching_departments.at(0)
 
     render({ :template => "departments/show.html.erb" })
   end
